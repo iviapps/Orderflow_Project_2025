34e7ded86f9f95001e5d5773f001e603f48fdda5
iviapps <iviapps.content@gmail.com>
2026-02-03 01:56:22 +0100

    fixed

--- Orderflow.Identity/Services/Auth/IAuthService.cs
+++ Orderflow.Identity/Services/Auth/IAuthService.cs
@@ -1,22 +1,19 @@
-﻿using System.Security.Claims;                                
-using Orderflow.Identity.DTOs.Auth;                          
-using Orderflow.Identity.DTOs;                               
+﻿using System.Security.Claims;
+using Orderflow.Identity.DTOs.Auth;
+using Orderflow.Identity.DTOs;
 using Orderflow.Identity.Services.Common;
 
-<<<<<<< HEAD
-namespace Orderflow.Identity.Services;                       
-    public interface IAuthService 
-=======
-namespace Orderflow.Identity.Services {                
+namespace Orderflow.Identity.Services
+{
     public interface IAuthService
->>>>>>> 4abee25c3f71540d33ac992f5c3162cbcc0e3498
     {
         Task<AuthResult<LoginResponse>> LoginAsync(
-            LoginRequest request);                            
+            LoginRequest request);
 
         Task<AuthResult<RegisterResponse>> RegisterAsync(
-            RegisterRequest request);                         
+            RegisterRequest request);
 
         Task<AuthResult<CurrentUserResponse>> GetCurrentUserAsync(
-            string userId);                                 
+            string userId);
     }
+}
