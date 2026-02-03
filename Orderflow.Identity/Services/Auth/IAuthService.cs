4a7dd28791d5ea71b16ad6e671a4cb0150e3a20a
iviapps <iviapps.content@gmail.com>
2026-02-03 03:46:55 +0100

    cambios

--- Orderflow.Identity/Services/Auth/IAuthService.cs
+++ Orderflow.Identity/Services/Auth/IAuthService.cs
@@ -3,7 +3,7 @@ using Orderflow.Identity.DTOs.Auth;
 using Orderflow.Identity.DTOs;                               
 using Orderflow.Identity.Services.Common;
 
-namespace Orderflow.Identity.Services                         
+namespace Orderflow.Identity.Services;                      
     public interface IAuthService
     {
         Task<AuthResult<LoginResponse>> LoginAsync(
@@ -15,4 +15,3 @@ namespace Orderflow.Identity.Services
         Task<AuthResult<CurrentUserResponse>> GetCurrentUserAsync(
             string userId);                                 
     }
-}
